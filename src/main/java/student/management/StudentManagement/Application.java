f05a9a5d925a49cdc09c773f66b5b57ec65e366b
Toshisue Hiroto <toshisuehiroto20020313@gmail.com>
2024-09-06 14:50:04 +0900

    Webアプリケーション課題１０回

--- src/main/java/student/management/StudentManagement/Application.java
+++ src/main/java/student/management/StudentManagement/Application.java
@@ -16,11 +16,7 @@ public class Application {
   @Autowired
   private StudentRepository repository;
 
-  //private String name = "Toshisue Hiroto";
-  //private String age = "22";
-  //private Map<String, String> studentNameMap = new HashMap<>(
-  // Map.of("1", "田中", "2", "斎藤", "3", "佐藤", "4", "広田", "5", "草薙"));
-  //変数名は絶対に必要だよ
+
 
   public static void main(String[] args) {
     //localhost:8080
@@ -29,16 +25,8 @@ public class Application {
   }
 
   @GetMapping("/studentList")
-  //この(/hello)が入力されたら↓のものを動かしますよってこと
+
   public List<Student> getStudentList() {
-    //パラメータで検索したものをとってくる
-
-    //Student student	= new Student();
-    //student.setName("Toshisue Hiroto");
-    //String name = student.getName();
-
-    //StringUtilsでも色々合ってapache commonsの方を使うたくさん機能がある　
-    //文字列を返す
     return repository.search();
 
 
@@ -49,34 +37,6 @@ public class Application {
     return repository.search2();
   }
 
-/*
-  @PostMapping("/student")
-  public void registerStudent(String name, int age) {
-    repository.registerStudent(name, age);
 
-    //二つPOSTで書きたい場合は↓こういう書き方をする。
-    //curl -X POST -d "name=Toshisue Hiroto&age=22" http://localhost:8080/studentInfo
-  }
-
-  @PatchMapping("/student")
-  public void updateStudent(String name, int age) {
-    //年齢だけだとすべての名前の人を変えてしまうからnameと年齢をしてする
-    repository.updateStudent(name,age);
-  }
-
-  @PostMapping("/studentNumber")
-  public void updateStudentNumber(String id, String name1) {
-    studentNameMap.put(id, name1);
-  }
-
-  //GET POST
-  //GETは取得する、リクエストの結果を受け取る、データを取得するときはGETを使う
-  //POSTは情報を与える、渡す　、データ登録や更新するときはPOSTを使う
-
-  @DeleteMapping("/student")
-  public void deleteStudent(String name){
-    repository.deleteStudent(name);
-  }
-*/
 
 }
--- src/main/java/student/management/StudentManagement/StudentRepository.java
+++ src/main/java/student/management/StudentManagement/StudentRepository.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
-//@Mapperをつけると自動的にMybatisが認識して処理してくれることを認識する
+
 public interface StudentRepository {
   @Select("SELECT * FROM students")
   //選択された名前を探してくる　検索処理
@@ -14,18 +14,7 @@ public interface StudentRepository {
 
   @Select("SELECT * FROM students_courses")
   List<StudentCourses> search2();
-/*
-  @Insert("INSERT student values(#{name},#{age})")
-  void registerStudent(String name, int age);
 
-
-  @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
-  void updateStudent(String name, int age);
-
-  @Delete("DELETE FROM student WHERE name = #{name}")
-  void deleteStudent(String name);
-
- */
 }
 
 
