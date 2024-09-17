07b5f08f44b8613c7b22ac4b3acedec3e755a72a
Toshisue Hiroto <toshisuehiroto20020313@gmail.com>
2024-09-17 16:19:15 +0900

    マージ

--- src/main/java/student/management/StudentManagement/controller/StudentController.java
+++ src/main/java/student/management/StudentManagement/controller/StudentController.java
@@ -3,10 +3,14 @@ package student.management.StudentManagement.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.naming.Binding;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import student.management.StudentManagement.controller.converter.StudentConverter;
 import student.management.StudentManagement.data.Student;
@@ -44,4 +48,22 @@ public class StudentController {
 
   }
 
+  @GetMapping("/newStudent")
+  public String newStudent(Model model){
+    model.addAttribute("studentDetail",new StudentDetail());
+    //空っぽのオブジェクトを設定して↓のregisterStudentでregisterStudent.htmlを表示
+    return "registerStudent";
+  }
+
+  @PostMapping("/registerStudent")
+  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result){
+    //registerStudent.htmlの紐づけを@ModelAttributeをつけることによりやってくれる
+    //thymeleafを使うときは、確実に行うもの　入力チェックしたその結果をBindinResultに格納して元の画面に返す
+    if(result.hasErrors()){
+      return  "registerStudent";
+    }
+    //System.out.println(studentDetail.getStudent().getName() + "さんが新規受講生として登録されました。");
+    return "redirect:/studentList";
+    //studentListに飛ばす
+  }
 }
